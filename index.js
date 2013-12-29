84ec3518a0c61059a5b72487276679d7cb8fc093
azu <azuciao@gmail.com>
2013-12-29 22:14:23 +0900

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var es = require('event-stream');
+var path = require("path");
+var autojumpDB = process.argv[2];
+if (autojumpDB == null) {
+    throw new Error("Usage : node index.jh /path/to/autojump.txt");
+}
+
+/**
+ * line by line
+ */
+function main() {
+    return es.map(function (line, cb) {
+        var split = line.split("\t");
+        var count = parseInt(split[0], 10),
+            dirPath = String(split[1]);
+        var output = "";
+        for(var i=0;i<count;i++) {
+            output += dirPath + "\n";
+        }
+        cb(null, output)
+    });
+}
+
+fs.createReadStream(path.resolve(autojumpDB), {flags: 'r'})
+    .pipe(es.split())
+    .pipe(main())
+    .pipe(process.stdout);
---
+++ readme.md
@@ -0,0 +1,23 @@
+# converter_from_autojump_to_percol
+
+Convert [autojump](https://github.com/joelthelion/autojump "autojump") db.txt to percol autojump.
+
+* [percol で z / autojump のようにディレクトリ高速ジャンプ - Slip Ahead Logging](http://stillpedant.hatenablog.com/entry/percol-cd-history "percol で z / autojump のようにディレクトリ高速ジャンプ - Slip Ahead Logging")
+
+## Usage
+
+``` sh
+node index.js /path/to/autojump.txt > .cd_history_file
+```
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## License
+
+MIT
